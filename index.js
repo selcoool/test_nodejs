cecaf498785ae70f8562d1375d2d0cbfbcc8f663
Tran Minh Thanh <trmthanh220895@gmail.com>
2024-07-10 01:56:23 +0800

    uP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World 22244444')
+  res.send('Hello World Dan Chi Sua Qua')
 })
 
 app.listen(port, () => {
